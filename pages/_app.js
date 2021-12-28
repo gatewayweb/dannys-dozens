1b45091ae9f168b880152353ec12df73c1310b13
Sam <sam@Sams-MacBook-Pro.local>
2021-12-27 22:55:02 -0500

    add social icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/globals.css';
 import Link from 'next/link';
+import '@fortawesome/fontawesome-svg-core/styles.css'; // import Font Awesome CSS
+import { config } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 import useStore, { updateLocalStorage } from '@/lib/store';
 
@@ -16,8 +22,23 @@ function App({ Component, pageProps }) {
       <Component {...pageProps} />
 
       <footer className="flex flex-col items-center justify-center w-full py-8 bg-gray-800 text-gray-300 font-light uppercase tracking-widest">
-        Copyright 2021 Dannysdozens.com
-        <div className="flex flex-wrap">
+        <div className="pb-2 flex flex-wrap">
+          <a target="_blank" href="https://www.instagram.com/dannysdozens/">
+            <FontAwesomeIcon
+              icon={faInstagram}
+              className="mx-3 duration-200 text-gray-300 hover:text-gray-100"
+              size="2x"
+            />
+          </a>
+          <a target="_blank" href="https://facebook.com">
+            <FontAwesomeIcon
+              icon={faFacebook}
+              className="mx-3 duration-200 text-gray-300 hover:text-gray-100"
+              size="2x"
+            />
+          </a>
+        </div>
+        <div className="flex flex-wrap pb-3">
           <Link href="/menu" passHref>
             <a className="text-sm block mx-2 mt-2 text-gray-100">Menu</a>
           </Link>
@@ -28,6 +49,7 @@ function App({ Component, pageProps }) {
             <a className="text-sm block mx-2 mt-2 text-gray-100">Contact</a>
           </Link>
         </div>
+        Copyright 2021 Dannysdozens.com
       </footer>
     </div>
   );
